4d3b853d4d425322d67fe42ff3caa506202d5342
Nadine <accentweb.com+codepixi.github@gmail.com>
2016-03-01 21:14:31 -0500

    External data test

---
+++ data/data.js
@@ -0,0 +1,22 @@
+	//http://www.jsoneditoronline.org/
+	var themes = {};
+	themes["cegep"] = {
+  "Projet graphique": 
+  {
+    "debut": "2016-03-1",
+    "fin": "2016-03-15",
+    "taches": 
+      {
+        "Corrections fonctionnel": 
+        {
+          "debut": "2016-03-1",
+          "fin": "2016-03-1"
+        },
+        "Corrections interfaces": 
+        {
+          "debut": "2016-03-3",
+          "fin": "2016-03-3"
+        }
+      }
+  }
+};
