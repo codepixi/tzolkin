1422f8f6f6fa1574f13d185c6cd91af10007d1c5
Nadine <accentweb.com+codepixi.github@gmail.com>
2016-03-01 21:26:50 -0500

    Color background

--- data/data.js
+++ data/data.js
@@ -5,6 +5,7 @@
   {
     "debut": "2016-03-1",
     "fin": "2016-03-15",
+	"couleur":"orange",
     "taches": 
       {
         "Corrections fonctionnel": 
@@ -16,6 +17,28 @@
         {
           "debut": "2016-03-3",
           "fin": "2016-03-3"
+        }
+      }
+  }
+};
+
+	themes["projet"] = {
+  "tzolkin": 
+  {
+    "debut": "2016-03-1",
+    "fin": "2016-03-30",
+	"couleur":"green",
+    "taches": 
+      {
+        "prototype d'affichage": 
+        {
+          "debut": "2016-03-5",
+          "fin": "2016-03-7"
+        },
+        "glisser-deposer": 
+        {
+          "debut": "2016-03-8",
+          "fin": "2016-03-12"
         }
       }
   }
